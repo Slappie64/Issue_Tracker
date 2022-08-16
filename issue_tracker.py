8b9cf35bdae2172837ce9e20c0e5dbb4461ec67a
Luigi Marino <emails@luigi-marino.com>
2022-08-16 10:28:26 +0000

    Able to create new issues in database

--- issue_tracker.py
+++ issue_tracker.py
@@ -4,7 +4,14 @@ from sqlalchemy import create_engine
 # Connection to database BIT.IO
 eng = create_engine('postgresql://Slappie64:v2_3tQZz_nUMRUk8r9BVqtfbZdcxtUGE@db.bit.io/Slappie64/issue_tracker', isolation_level='AUTOCOMMIT')
 
+def new_issue(uid, name, date, project):
+	conn.execute("INSERT INTO issues (UID, Name, Date, Project, Status) VALUES ('{}','{}','{}', '{}', 'Not Started');".format(uid, name, date, project))
+
+
 # With the connection open...
 with eng.connect() as conn:
-    result = conn.execute("SELECT 'Hello bit.io!';")
-    print(result.first()[0])
+    try:
+        new_issue('1','Test', '17/08/2022', 'Issue_Tracker')
+    except Exception as ex:
+        print("Error: ", ex)
+
