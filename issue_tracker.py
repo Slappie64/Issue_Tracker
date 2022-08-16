13194863d25b43feea09f8934b17b2573f671224
Luigi Marino <emails@luigi-marino.com>
2022-08-16 10:52:45 +0000

    Automatically increment UID on new tasks

--- issue_tracker.py
+++ issue_tracker.py
@@ -4,14 +4,19 @@ from sqlalchemy import create_engine
 # Connection to database BIT.IO
 eng = create_engine('postgresql://Slappie64:v2_3tQZz_nUMRUk8r9BVqtfbZdcxtUGE@db.bit.io/Slappie64/issue_tracker', isolation_level='AUTOCOMMIT')
 
-def new_issue(uid, name, date, project):
-	conn.execute("INSERT INTO issues (UID, Name, Date, Project, Status) VALUES ('{}','{}','{}', '{}', 'Not Started');".format(uid, name, date, project))
+# Automatically increment UID
+def auto_increment():
+    result = conn.execute("SELECT MAX(UID) FROM issues;")
+    return result.fetchone()[0] + 1
 
+# Create a new issue in the DB
+def new_issue(name, date, project):
+	conn.execute("INSERT INTO issues (UID, Name, Date, Project, Status) VALUES ('{}','{}','{}', '{}', 'Not Started');".format(auto_increment(), name, date, project))
 
 # With the connection open...
 with eng.connect() as conn:
     try:
-        new_issue('1','Test', '17/08/2022', 'Issue_Tracker')
+        new_issue('Test3', '19/08/2022', 'Issue_Tracker')
     except Exception as ex:
         print("Error: ", ex)
 
