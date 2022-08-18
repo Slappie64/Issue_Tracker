a1d159080f512378afc931806c2d67b5adb0d313
Luigi Marino <emails@luigi-marino.com>
2022-08-18 10:56:43 +0000

    Input variables for New, List and Update functions

--- issues.py
+++ issues.py
@@ -6,35 +6,52 @@ app_description = """
     This is a test and a placeholder.
 """
 
-parser = argparse.ArgumentParser(description = app_description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description = app_description, prog='Issue Tracker')
 
-parser.add_argument('-n', '--new', help='Create a new issue.')
-parser.add_argument('-l', '--list', help='List all issues')
+#group = parser.add_mutually_exclusive_group()
+
+subparsers = parser.add_subparsers(help='New')
+
+new_parser = subparsers.add_parser('new', help='Create a new issue.')
+new_parser.add_argument('-n', '--name', action='store', dest='issue_name', help='New Issue Name.')
+new_parser.add_argument('-d', '--due', action='store', dest='issue_due', help='New Issue Due Date.')
+
+update_parser = subparsers.add_parser('update', help='Update an exisiting issue.')
+update_parser.add_argument('-uid', action='store', dest='issue_uid', help='UID of issue to update.')
+
+
+list_parser = subparsers.add_parser('list', help='List issues.')
+list_parser.add_argument('-p', '--project', action='store', dest='issue_project', help='Filter by project.')
+list_parser.add_argument('-d', '--due', action='store', dest='issue_due', help='Filter by due date.')
+
+
+#group.add_argument('-n', '--new', action='store', dest='issue_name', help='Create a new issue.')
+#group.add_argument('-l', '--list', action='store_true', help='List all issues.')
+#group.add_argument('-u', '--update', action='store', dest='issue_UID', help='Update issue, requires issue ID')
+
+
+
 args = parser.parse_args()
 config = vars(args)
 
-print(config)
-
-# Get CLI Arguments
-#flag = sys.argv[1]
-
+def line_break():
+    print('-'*20)
 
 """
-# Check incoming flags
-if flag == '-n':
-    print('Create New Issue')
-
-
-def new_issue():
-    new_issue_name = input('Name: ')
-    new_issue_due = input('Due: ')
-    new_issue_project = input('Project: ')
-    new_issue_details = input('Details: ')
-    # Add to Database
-    # Log success/fail
-
-def update_issue():
-
 # TEST PRINTS
-print(n)
+# print(n)
+print(config)
+print(args)
+line_break()
+print(subparsers)
+line_break()
+print(new_parser)
+print(update_parser)
+print(list_parser)
+
+#print(subparsers)
+
+for i in config:
+    print(i)
+
 """
