e73b77d0d0f5da5646fd5e9e779df8a01b3b13ce
Luigi Marino <emails@luigi-marino.com>
2022-08-23 13:35:27 +0000

    Created function format_task_info() to normalise the incoming data into a class for easy access to each element

--- tasks.py
+++ tasks.py
@@ -12,12 +12,12 @@ parser = argparse.ArgumentParser(description='Task/Issue Tracker')
 args = parser.parse_args()
 
 # Database connection
-eng = create_engine('postgresql://Slappie64:v2_3tQZz_nUMRUk8r9BVqtfbZdcxtUGE@db.bit.io/Slappie64/issue_tracker', isolation_level='AUTOCOMMIT')
+eng = create_engine('postgresql://Slappie64:v2_3taP7_NuF8dCDxLq7v8CMhstQs7gN@db.bit.io/Slappie64/issue_tracker', isolation_level='AUTOCOMMIT')
 
 # Class - Custom class for tasks and the relevant variables
 class Task:
-    def __init__(self, UID, name, due):
-        self.UID = UID
+    def __init__(self, uid, name, due):
+        self.uid = uid
         self.name = name
         self.due = due
 
@@ -38,12 +38,13 @@ def specific_time_weeks(amount):
     return datetime.now() + timedelta(weeks=amount)
 
 
-def format_task_info(task):
-    print(task.UID)
-    print(task.name)
-    print(task.due)
-    print(task.due)
-    print(task.created)
+def format_task_info(in_task):
+
+    format_task = Task(in_task['uid'], in_task['name'], in_task['date'])
+    #for i in in_task.keys():
+    #    print(i, in_task[i])
+    #print('-'*10)
+    return format_task
 
 def auto_increment():
     result = conn.execute("SELECT MAX(UID) FROM issues;")
@@ -55,13 +56,15 @@ task01 = Task(1, 'test1', specific_time_days(3))
 task02 = Task(2, 'test2', specific_time_hours(36))
 task03 = Task(3, 'test3', next_week)
 
-format_task_info(task01)
-format_task_info(task02)
-format_task_info(task03)
+#format_task_info(task01)
+#format_task_info(task02)
+#format_task_info(task03)
 
 if __name__ == '__main__':
     with eng.connect() as conn:
         task_list = conn.execute("SELECT * FROM issues;")
         for i in task_list:
-            for key in i.keys():
-                print(key, i[key])
+            print(format_task_info(i).uid)
+            print(format_task_info(i).name)
+            print(format_task_info(i).due)
+            print('-'*10)
